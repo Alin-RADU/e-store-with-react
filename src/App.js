bebabb85385a9be6207fb8fe76145f23122873e4
Alin RADU <alin.radu.cristian@gmail.com>
2020-10-15 15:00:46 +0300

    created- HomePage, MenuItems, MenuItem, finished styling MenuItem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import HomePage from './components/HomePage';
+import HomePage from './pages/homepage/HomePage';
+
+import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="body">
       <HomePage />
     </div>
   );
