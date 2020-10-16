255253e94e4ee122c2e9f2eeea408b23837a8510
Alin RADU <alin.radu.cristian@gmail.com>
2020-10-16 11:17:11 +0300

    created- Header, added links to Home and ShopPage

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/HomePage/HomePage';
 import ShopPage from './pages/ShopPage/ShopPage';
+import Header from './components/Header/Header';
 
 import './App.css';
 
 const App = () => {
   return (
     <div className="body">
+      <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/shop" exact component={ShopPage} />
